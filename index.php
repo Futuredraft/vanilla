eb376136301f5cb7d339c2178d06a025c3883578
infinityplusone <jonathan@infinityplusone.com>
2015-05-05 22:07:43 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+<title>Futuredraft</title>
+<style type="text/css">
+a {
+  background: #fff url(http://www.futuredraft.com/assets/img/fd_logo_333x25.svg) center center no-repeat;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  text-indent: 100%;
+  white-space: nowrap;
+}
+</style>
+</head>
+<body>
+<a href="http://futuredraft.com/" title="Futuredraft">Futuredraft</a>
+</body>
+</html>
